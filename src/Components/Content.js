3816124c6a897f32e9f3c7dbdcd3f6b5c56a41ac
Surya Reddy <surya.reddy@medlife.com>
2018-04-30 19:33:46 +0530

    final commit

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -19,16 +19,50 @@ export default class Content extends React.Component {
     );
   }
 
+  onChange = (event, newValue) => {
+    event.preventDefault();
+    const {onChange, currentIndex} = this.props;
+
+    onChange(currentIndex, newValue);
+  }
+
   _renderAnswer() {
+    const {questionDetails = {}} = this.props;
+    const {question_type} = questionDetails;
+
+    switch(question_type) {
+      case 'TextQuestion':
+        return this._renderTextAnswer();
+      case 'Radio':
+      case 'Checkbox':
+      default:
+        return this._renderTextAnswer();  
+    }
+  }
+
+  _renderTextAnswer() {
     const {questionDetails = {}} = this.props;
     const {question_type, is_required, min_char_length} = questionDetails;
     let {answer = ''} = questionDetails;
+    let errorText = '';
+
+    if (answer.length < min_char_length) {
+      errorText = `Min character length is ${min_char_length}`;
+    }
+
+    if (is_required && answer === '') {
+      errorText = 'This field is required';
+    }
 
     return (
       <TextField
         hintText="Type in answer"
         multiLine={true}
         rows={7}
+        value={answer}
+        fullWidth={true}
+        onChange={this.onChange}
+        errorText={errorText}
       />
     );
   }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -15,26 +15,24 @@ export default class Form extends React.Component {
           "question_type": "TextQuestion",
           "prompt": "What is your first answer?",
           "is_required": false,
-          "min_char_length": 15
+          "min_char_length": 0
         },
         {
           "id": 2,
           "question_type": "TextQuestion",
           "prompt": "What is your second answer?",
           "is_required": true,
-          "min_char_length": 100
+          "min_char_length": 10
         },
         {
           "id": 3,
           "question_type": "TextQuestion",
           "prompt": "What is your third answer?",
           "is_required": true,
-          "min_char_length": 1,
+          "min_char_length": 3,
         }
       ],
-      currentIndex: 0,
-      enableNext: true,
-      enablePrev: false
+      currentIndex: 0
     }
   }
 
@@ -42,42 +40,73 @@ export default class Form extends React.Component {
     let currentIndex = this.state.currentIndex - 1;
     
     this.setState({
-      currentIndex: currentIndex,
-      enablePrev: currentIndex !== 0
+      currentIndex: currentIndex
     });
   }
 
   onNextClick = () => {
     let currentIndex = this.state.currentIndex + 1;
 
-    if (currentIndex < this.state.questions.length) {
-      this.setState({
-        currentIndex: currentIndex,
-        enablePrev: currentIndex !== 0,
-      });
-    } else {
-      this.setState({
-        enableNext: false
-      });
+    this.setState({
+      currentIndex: currentIndex,
+    });
+  }
+
+  handleChange = (index, value) => {
+    let {questions} = this.state;
+
+    questions[index].answer = value;
+    this.setState({
+      questions
+    });
+  }
+
+  shouldEnableNext() {
+    const {questions, currentIndex} = this.state;
+    const question = questions[currentIndex];
+    const value = question.answer || '';
+
+    switch(question.question_type) {
+      case 'TextQuestion':
+        let isRequiredMet = true;
+        if (question.is_required && value === '') {
+          isRequiredMet = false;
+        }
+        const isLengthMet = value.length >= (question.min_char_length || 0);
+
+        return isRequiredMet && isLengthMet && currentIndex < questions.length - 1;
+
+      case 'Radio':
+        // Logic to write support for Radio question
+        return true;
+
+      case 'Checkbox':
+        // Logic to write support for multiple answer question
+        return true;
+
+      default:
+        return true;
     }
   }
 
   render() {
-    const {title, questions, currentIndex, enableNext, enablePrev} = this.state;
+    const {title, questions, currentIndex} = this.state;
+    const enableNext = this.shouldEnableNext();
 
     return (
       <Card>
         <div className="container">
           <Header title={title} />
-          <div>
+          <div className="content-wrapper">
             <Content
               questionDetails={questions[currentIndex]}
               currentIndex={currentIndex}
+              onChange={this.handleChange}
             />
           </div>
           <Footer
             enableNext={enableNext}
-            enablePrev={enablePrev}
+            enablePrev={currentIndex !== 0}
             onNextClick={this.onNextClick}
             onPrevClick={this.onPrevClick}
           />
